316c963cd58cd5f6a3381b449b4d0d91d5216c8f
wooawi <dinara5.zinatulina@gmail.com>
2026-05-27 15:50:31 +0700

    персонажи

---
+++ novella/Character.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solution.CharacterManager
+{
+    public class GetCharacter(id)
+    {
+
+    }
+
+    public class UpdateCharacter(id)
+    {
+
+    }
+
+    public class KillCharacter(id)
+    {
+
+    }
+
+    public class GetAllCharacter()
+    {
+
+    }
+    //хранит персонажей.следит за состоянием пероснажей. изменяет характеристики персонажей
+}
