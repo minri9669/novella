dda588a73ad2e90771c67d1209a39a396f8f6272
AfaAe <kell2281815d@gmail.com>
2026-06-20 13:39:18 +0700

    надеюсь всё добавила

---
+++ DialogMnager/BaseText.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project.DialogMnager
+{
+    using System.Collections.Generic;
+    namespace бурмаджа_.dialogue
+
+    {
+        public class BaseText
+        {
+            private DialogueManager dm = new DialogueManager();
+
+            public List<DialogueLine> GetDialog(int a)
+            {
+                return dm.GetDialog(a);
+            }
+            public BaseText()
+            {
+                DialogueLine a1 = new DialogueLine("Автор", "ХАНТАВИРУС: ЧЕРНОБЫЛЬСКАЯ ПЕТЛЯ", 1, 0); dm.AddText(a1, 0);
+                DialogueLine a2 = new DialogueLine("Автор", "2067 год. Чернобыль. Восстановленный город.\nВертолёт МЧС приземлился на площадке у четвёртого энергоблока.\n" +
+                    "\"Из него вышли четыре женщины в тактических куртках, с рюкзаками за плечами.\nЛучшие из лучших. Москва отправила их выяснить настоящую причину аварии 1986 года.\n" +
+                    "Эмма, Диана, Роня и Динара знали друг друга не первый год.", 0, 1); dm.AddText(a2, 0);
+                DialogueLine a3 = new DialogueLine("Автор", "Эмма — патологоанат, тихая и наблюдательная.\nДиана — фельдшер скорой, лидер, холодная и рациональная.\nРоня — хирург, резкая, физически сильная, с револьвером в кобуре. Динара — медсестра, циничная, золотые руки.", 0, 1); dm.AddText(a3, 0);
+                DialogueLine a4 = new DialogueLine("Автор", "Никто туда не спускался полвека. Спуститься предстоит им.\nОни вошли внутрь.", 1, 0); dm.AddText(a4, 0);
+
+                DialogueLine b = new DialogueLine("саиасипаси", "ывапываыва", 1, 0); dm.AddText(b, 1);
+                DialogueLine b1 = new DialogueLine("чвапмвчап", "вввввввввввафыаввыа", 0, 1); dm.AddText(b1, 1);
+                DialogueLine b2 = new DialogueLine("ыфваяваптва", "ываываывпаявчввпиваа", 1, 0); dm.AddText(b2, 1);
+            }
+        }
+        public class DialogueLine
+        {
+            public string Name { get; set; }
+            public string Text { get; set; }
+            public int indIm { get; set; }
+            public int intImB { get; set; }
+
+            public DialogueLine(string name, string text, int imageIndex, int backgroundIndex)
+            {
+                Text = text;
+                Name = name;
+                indIm = imageIndex;
+                intImB = backgroundIndex;
+            }
+
+        }
+        public class DialogueManager
+        {
+            private List<string> history = new List<string>();
+            private List<List<DialogueLine>> listOfLists = new List<List<DialogueLine>>();
+            public void AddText(DialogueLine line, int a)
+            {
+                if (listOfLists.Count - 1 < a)
+                {
+                    listOfLists.Add(new List<DialogueLine>());
+                }
+                listOfLists[a].Add(line);
+            }
+
+            public List<DialogueLine> GetDialog(int a)
+            {
+                return listOfLists[a];
+            }
+            public void TextWindow(DialogueLine line)
+            {
+                string kukaracha = line.Name + ": " + line.Text;
+                history.Add(kukaracha);
+            }
+            public List<string> GetHistory()
+            {
+                return history;
+            }
+            public void ClearHistory()
+            {
+                history.Clear();
+            }
+
+        }
+    }
+}
---
+++ MainGaameH/MainForm.cs
@@ -0,0 +1,277 @@
+﻿using project.DialogMnager.бурмаджа_.dialogue;
+using project.EndingManager.EndingUsee;
+using project.MiniGame3.HelpClass;
+using project.MiniGame3.Skip;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project
+{
+    public partial class MainFormGame : Form
+    {
+        private SkipDialogs skipd = new SkipDialogs();
+        private BaseText bt = new BaseText();
+        private List<DialogueLine> currentDialogList;
+        private int currentDialogIndex = 0;
+        private bool isDialogPlaying = false;
+        private bool isWaitingForEnter = false;
+        private string currentFullText = "";
+        private int currentSprites = -1;
+        private int currentBackground = -1;
+
+        private List<Image> SpritesImages;
+        private List<Image> BackgroundImages;
+
+        EndingUse eu = new EndingUse();
+        public MainFormGame()
+        {
+            InitializeComponent();
+            LoadImagesToList();
+            panel1.Parent = pictureBox1;
+            label1.Parent = pictureBox1;
+            pictureBox2.Parent = pictureBox1;
+            panel1.BackColor = Color.FromArgb(128, 0, 0, 0);
+            label1.BackColor = Color.FromArgb(128, 0, 0, 0);
+
+            panel1.Visible = true;
+            label1.Visible = true;
+            pictureBox2.Visible = true;
+            panel2.Visible = false;
+
+            skipd.OnTextUpdated += (text) =>
+            {
+                if (label2.InvokeRequired)
+                    label2.Invoke(new Action(() => label2.Text = text));
+                else
+                    label2.Text = text;
+            };
+
+            skipd.OnTypingComplete += () =>
+            {
+                isWaitingForEnter = true;
+
+            };
+
+            timer1.Start();
+            this.KeyDown += MainFormGame_KeyDown;
+            this.Shown += MainFormGame_Shown;
+            this.Select();
+            
+        }
+        private void MainFormGame_Shown(object sender, EventArgs e)
+        {
+            panel1.Visible = true;
+            label1.Visible = true;
+            pictureBox2.Visible = true;
+            label2.Visible = true;
+
+            currentDialogList = bt.GetDialog(0);
+            currentDialogIndex = 0;
+            isDialogPlaying = true;
+            currentSprites = -1;
+            currentBackground = -1;
+            currentFullText = "";
+
+            ShowNextDialog();
+        }
+
+        //Диалоги и скип
+        private void LoadImagesToList()
+        {
+            SpritesImages = new List<Image>
+            {
+                //Properties.Resources.image1,
+                //Properties.Resources.image2
+            };
+
+            BackgroundImages = new List<Image>
+            {
+                //Properties.Resources.BackgroundMiniGame2,
+                //Properties.Resources.KoncoBkuBackgroind
+            };
+        }
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            if (keyData == Keys.Enter)
+            {
+                HandleEnterPress();
+                return true;
+            }
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+        private void MainFormGame_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                HandleEnterPress();
+                e.Handled = true;
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private void HandleEnterPress()
+        {
+            if (skipd.IsTyping())
+            {
+                skipd.Skip();
+                currentFullText = skipd.GetFullText();
+
+                if (label2.InvokeRequired)
+
+
+                    isWaitingForEnter = true;
+                return;
+            }
+
+            if (isWaitingForEnter)
+            {
+                isWaitingForEnter = false;
+                if (label2.InvokeRequired)
+                    label2.Invoke(new Action(() =>
+                    {
+                        string currentText = label2.Text;
+                        label2.Text = currentText;
+                    }));
+                else
+                {
+                    string currentText = label2.Text;
+                    label2.Text = currentText;
+                }
+                ShowNextDialog();
+            }
+        }
+
+        private void ShowNextDialog()
+        {
+            if (currentDialogIndex < currentDialogList.Count)
+            {
+                var dialog = currentDialogList[currentDialogIndex];
+                string be;
+                if (dialog.Name == "Автор") { be = dialog.Text + "\n\n"; }
+                else { be = dialog.Name + ":  " + dialog.Text + "\n\n"; }
+                richTextBox1.Text += be;
+                if (dialog.Name == "Автор")
+                {
+                    pictureBox2.Visible = false;
+                    if (label1.InvokeRequired)
+                        label1.Invoke(new Action(() => label1.Visible = false));
+                    else
+                        label1.Visible = false;
+                }
+                else
+                {
+                    pictureBox2.Visible = true;
+                    if (label1.InvokeRequired)
+                        label1.Invoke(new Action(() =>
+                        {
+                            label1.Visible = true;
+                            label1.Text = dialog.Name;
+                        }));
+                    else
+                    {
+                        label1.Visible = true;
+                        label1.Text = dialog.Name;
+                    }
+                }
+                if (dialog.indIm != currentSprites)
+                {
+                    currentSprites = dialog.indIm;
+                    if (pictureBox2.InvokeRequired)
+                    {
+                        pictureBox2.Invoke(new Action(() =>
+                        {
+                            if (dialog.indIm >= 0 && dialog.indIm < SpritesImages.Count)
+                                pictureBox2.Image = SpritesImages[dialog.indIm];
+                        }));
+                    }
+                    else
+                    {
+                        if (dialog.indIm >= 0 && dialog.indIm < SpritesImages.Count)
+                            pictureBox2.Image = SpritesImages[dialog.indIm];
+                    }
+                }
+
+                if (dialog.intImB != currentBackground)
+                {
+                    currentBackground = dialog.intImB;
+                    if (pictureBox1.InvokeRequired)
+                    {
+                        pictureBox1.Invoke(new Action(() =>
+                        {
+                            if (dialog.intImB >= 0 && dialog.intImB < BackgroundImages.Count)
+                                pictureBox1.Image = BackgroundImages[dialog.intImB];
+                        }));
+                    }
+                    else
+                    {
+                        if (dialog.intImB >= 0 && dialog.intImB < BackgroundImages.Count)
+                            pictureBox1.Image = BackgroundImages[dialog.intImB];
+                    }
+                }
+
+                if (label2.InvokeRequired)
+                    label2.Invoke(new Action(() => label2.Text = ""));
+                else
+                    label2.Text = "";
+
+                currentFullText = "";
+
+                skipd.StartTyping(dialog.Text, 30);
+                currentDialogIndex++;
+                isWaitingForEnter = false;
+            }
+            else
+            {
+                if (panel1.InvokeRequired)
+                    panel1.Invoke(new Action(() =>
+                    {
+                        panel1.Visible = false;
+                        label1.Visible = false;
+                        pictureBox2.Visible = false;
+                        pictureBox1.Image = Properties.Resources.backgroundMiniGame2;
+
+                    }));
+                else
+                {
+                    panel1.Visible = false;
+                    label1.Visible = false;
+                    pictureBox2.Visible = false;
+                    pictureBox1.Image = Properties.Resources.backgroundMiniGame2;
+                }
+
+                isDialogPlaying = false;
+                isWaitingForEnter = false;
+                currentFullText = "";
+
+                this.Select();
+            }
+        }
+
+
+
+        //Концовки
+        private void button1_Click(object sender, EventArgs e)
+        {
+            EndingsLabel2.Text = eu.ShowAll();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EndingsLabel2.Text = eu.ShowAllUnlocked();
+        }
+
+        public void Unlocking(string a)
+        {
+            eu.Unlocking(a);
+        }
+    }
+}
